a2c9b821cc9be7ef22996dfa63d0b352d8973c94
ialford <ialford@nd.edu>
2020-10-08 16:32:13 -0400

    fix unit test

--- test/foundation-stack.test.ts
+++ test/foundation-stack.test.ts
@@ -8,6 +8,27 @@ test('Empty Stack', () => {
   const stack = new FoundationStack(app, 'MyFoundationStack')
   // THEN
   expectCDK(stack).to(matchTemplate({
-    Resources: {},
+    Resources: {
+      logBucket1FE17E85: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          AccessControl: 'LogDeliveryWrite',
+          LifecycleConfiguration: {
+            Rules: [
+              {
+                ExpirationInDays: 365,
+                NoncurrentVersionExpirationInDays: 1,
+                Status: 'Enabled',
+              },
+            ],
+          },
+          VersioningConfiguration: {
+            Status: 'Enabled',
+          },
+        },
+        UpdateReplacePolicy: 'Retain',
+        DeletionPolicy: 'Retain',
+      },
+    },
   }, MatchStyle.EXACT))
 })
