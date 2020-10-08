b00592bec496425e7d43287af4872ea6ca015eac
ialford <ialford@nd.edu>
2020-10-08 15:56:50 -0400

    add log bucket to foundation

--- src/foundation-stack.ts
+++ src/foundation-stack.ts
@@ -4,7 +4,6 @@ import { Bucket, BucketAccessControl } from '@aws-cdk/aws-s3'
 export interface FoundationStackProps extends cdk.StackProps {}
 
 export class FoundationStack extends cdk.Stack {
-
   public readonly logBucket: Bucket
 
   constructor (scope: cdk.Construct, id: string, props?: FoundationStackProps) {
@@ -12,14 +11,14 @@ export class FoundationStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    this.logBucket = new Bucket(this, 'logBucket',{
+    this.logBucket = new Bucket(this, 'logBucket', {
       accessControl: BucketAccessControl.LOG_DELIVERY_WRITE,
       versioned: true,
-      lifecycleRules:[{
+      lifecycleRules: [{
         enabled: true,
         noncurrentVersionExpiration: cdk.Duration.days(1),
         expiration: cdk.Duration.days(365),
       }],
-      })
+    })
   }
 }
