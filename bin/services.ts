e94c8925ef90606d28bcb298ec2fa2c66f1cabd1
ialford <ialford@nd.edu>
2020-10-09 11:21:05 -0400

    add beehive service stack

--- bin/services.ts
+++ bin/services.ts
@@ -6,10 +6,17 @@ import { FoundationStack } from '../lib/foundation-stack'
 import { HoneycombStack } from '../lib/honeycomb-stack'
 import { HoneypotStack } from '../lib/honeypot-stack'
 import { Stacks } from '../lib/types'
+import { getContextByNamespace } from '../lib/context-helpers'
+
+const beehiveContext = getContextByNamespace('beehive')
 
 export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment): Stacks => {
   const foundationStack = new FoundationStack(app, `${namespace}-foundation`, { env })
-  const beehiveStack = new BeehiveStack(app, `${namespace}-beehive`, { env, foundationStack })
+  const beehiveStack = new BeehiveStack(app, `${namespace}-beehive`, {
+    env,
+    foundationStack,
+    ...beehiveContext,
+  })
   const buzzStack = new BuzzStack(app, `${namespace}-buzz`, { env, foundationStack })
   const honeycombStack = new HoneycombStack(app, `${namespace}-honeycomb`, { env, foundationStack })
   const honeypotStack = new HoneypotStack(app, `${namespace}-honeypot`, { env, foundationStack })
--- src/beehive-stack.ts
+++ src/beehive-stack.ts
@@ -1,12 +1,64 @@
 import * as cdk from '@aws-cdk/core'
+import { CloudFrontAllowedMethods, CloudFrontWebDistribution, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront'
+import { Bucket } from '@aws-cdk/aws-s3'
 import { SharedServiceStackProps } from './shared-stack-props'
+import { FoundationStack } from './foundation-stack'
 
-export interface BeehiveStackProps extends SharedServiceStackProps {}
+export interface BeehiveStackProps extends SharedServiceStackProps {
+  readonly hostnamePrefix: string,
+  readonly foundationStack: FoundationStack
+}
 
 export class BeehiveStack extends cdk.Stack {
+  public readonly hostname: string
+  public readonly bucket: Bucket
+  public readonly cloudfront: CloudFrontWebDistribution
+
   constructor (scope: cdk.Construct, id: string, props: BeehiveStackProps) {
     super(scope, id, props)
 
     // The code that defines your stack goes here
+
+    this.hostname = `${props.hostnamePrefix || this.stackName}`
+    const webBucket = new Bucket(this, 'beehiveBucket', {
+      serverAccessLogsBucket: props.foundationStack.logBucket,
+      serverAccessLogsPrefix: `s3/${this.hostname}`,
+    })
+
+    // TODO: [ESU-1474] add proper SSL certificate to distribution
+    
+    this.cloudfront = new CloudFrontWebDistribution(this, 'beehiveDistrobution', {
+      comment: this.hostname,
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: webBucket,
+          originAccessIdentity: new OriginAccessIdentity(this, 'beehiveOAI', {
+            comment: `Static Assets in ${webBucket.bucketName}`,
+          }),
+        },
+        behaviors: [{
+          allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+          isDefaultBehavior: true,
+          defaultTtl: (props.env === 'dev') ? cdk.Duration.seconds(0) : cdk.Duration.days(1),
+        }],
+      }],
+      loggingConfig: {
+        bucket: props.foundationStack.logBucket,
+        includeCookies: true,
+        prefix: `web/${this.hostname}`,
+      },
+      errorConfigurations: [
+        {
+          errorCode: 404,
+          responseCode: 200,
+          responsePagePath: '/',
+        },
+        {
+          errorCode: 403,
+          responseCode: 200,
+          responsePagePath: '/',
+        },
+      ],
+    })
   }
 }
