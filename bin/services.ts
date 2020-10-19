2ab3c06bf99d86eb53a948e5b3d8aabb67c9eeca
ialford <ialford@nd.edu>
2020-10-19 09:50:22 -0400

    update for commonProps

--- bin/services.ts
+++ bin/services.ts
@@ -8,16 +8,28 @@ import { HoneypotStack } from '../lib/honeypot-stack'
 import { Stacks } from '../lib/types'
 import { getContextByNamespace } from '../lib/context-helpers'
 
-const beehiveContext = getContextByNamespace('beehive')
-
 export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment): Stacks => {
+  // Construct common props that are required by all service stacks
+  const commonProps = {
+    namespace,
+    account: env.account,
+    region: env.region,
+    useVpcId: env.useVpcId,
+    contextEnvName: env.name,
+    createDns: env.createDns,
+    domainStackName: env.domainStackName,
+
+  }
   const domainStackName = app.node.tryGetContext('domainStack')
-  const foundationStack = new FoundationStack(app, `${namespace}-foundation`, { env, domainStackName })
+  const foundationStack = new FoundationStack(app, `${namespace}-foundation`, { ...commonProps })
+
+  const beehiveContext = getContextByNamespace('beehive')
   const beehiveStack = new BeehiveStack(app, `${namespace}-beehive`, {
-    env,
     foundationStack,
+    ...commonProps,
     ...beehiveContext,
   })
+
   const buzzStack = new BuzzStack(app, `${namespace}-buzz`, { env, foundationStack })
   const honeycombStack = new HoneycombStack(app, `${namespace}-honeycomb`, { env, foundationStack })
   const honeypotStack = new HoneypotStack(app, `${namespace}-honeypot`, { env, foundationStack })
