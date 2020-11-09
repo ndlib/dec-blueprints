6e03d379cb807e2184f47bcda519abf8186a3819
Bill Sill <wsill@nd.edu>
2020-11-09 09:15:06 -0500

    Hard code the rails run env value to production

--- src/honeypot-stack.ts
+++ src/honeypot-stack.ts
@@ -61,14 +61,6 @@ export class HoneypotStack extends cdk.Stack {
       return ecs.Secret.fromSsmParameter(parameter)
     }
 
-    const stringHelper = (task: string, key: string) => {
-      const parameter = ssm.StringParameter.fromStringParameterAttributes(this, `${task}${key}`, {
-        parameterName: `/all/${props.hostnamePrefix}/${key}`,
-        version: 1, // This doesn't seem to matter in the context of ECS task definitions
-      })
-      return parameter.stringValue
-    }
-
     // ECS Service
     const cluster = new ecs.Cluster(this, 'FargateCluster', { vpc })
 
@@ -102,7 +94,7 @@ export class HoneypotStack extends cdk.Stack {
         SECRET_KEY_BASE: secretsHelper('HoneypotService', 'secret_key_base'),
       },
       environment: {
-        RAILS_RUN_ENV: stringHelper('HoneypotService', 'rails_run_env'),
+        RAILS_RUN_ENV: "production",
       },
 
     })
