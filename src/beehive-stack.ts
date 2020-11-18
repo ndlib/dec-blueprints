b19e4f0047a3622aa3aaf94bc08fbe5381cd2929
Tom Hanstra <hanstra@nd.edu>
2020-11-18 10:08:23 -0500

    Add BucketDeployment

--- src/beehive-stack.ts
+++ src/beehive-stack.ts
@@ -5,6 +5,7 @@ import { Bucket } from '@aws-cdk/aws-s3'
 import { SharedServiceStackProps } from './shared-stack-props'
 import { FoundationStack } from './foundation-stack'
 import { CustomEnvironment } from './custom-environment'
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
 
 export interface BeehiveStackProps extends SharedServiceStackProps {
   readonly hostnamePrefix: string,
@@ -14,8 +15,8 @@ export interface BeehiveStackProps extends SharedServiceStackProps {
 
 export class BeehiveStack extends cdk.Stack {
   public readonly hostname: string
-  public readonly bucket: Bucket
-  public readonly cloudfront: CloudFrontWebDistribution
+//  public readonly bucket: Bucket
+//  public readonly cloudfront: CloudFrontWebDistribution
 
   constructor (scope: cdk.Construct, id: string, props: BeehiveStackProps) {
     super(scope, id, props)
@@ -32,7 +33,7 @@ export class BeehiveStack extends cdk.Stack {
       serverAccessLogsPrefix: `s3/${this.hostname}`,
     })
 
-    this.cloudfront = new CloudFrontWebDistribution(this, 'beehiveDistribution', {
+    const distribution = new CloudFrontWebDistribution(this, 'beehiveDistribution', {
       comment: this.hostname,
       aliasConfiguration: {
         names: [this.hostname + '.' + cdk.Fn.importValue(`${props.env.domainStackName}:DomainName`)],
@@ -79,5 +80,12 @@ export class BeehiveStack extends cdk.Stack {
         ttl: cdk.Duration.minutes(15),
       })
     }
+    const appSourcePath = "../beehive/build";
+
+    new BucketDeployment(this, 'DeployWebsite', {
+      sources: [Source.asset(appSourcePath)],
+      destinationBucket: webBucket,
+      distribution: distribution,
+    });
   }
 }
