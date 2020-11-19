bed9af42b3903bf112d92ca5efe08329e4d3127c
Tom Hanstra <hanstra@nd.edu>
2020-11-19 11:16:58 -0500

    Add commonProps and Foundation Stack; additional imports

--- bin/pipelines.ts
+++ bin/pipelines.ts
@@ -1,8 +1,22 @@
 import { App } from '@aws-cdk/core'
+import { BeehiveStack } from '../lib/beehive-stack'
+import { BeehivePipelineStack} from '../lib/beehive-pipeline'
 import { CustomEnvironment } from '../lib/custom-environment'
+import { FoundationStack } from '../lib/foundation-stack'
+import { getContextByNamespace } from '../lib/context-helpers'
 import { Stacks } from '../lib/types'
 
 export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment, testStacks: Stacks, prodStacks: Stacks): Stacks => {
+
+  // Construct common props that are required by all service stacks
+  const commonProps = {
+    namespace,
+    env: env,
+  }
+
+  const foundationStack = new FoundationStack(app, `${namespace}-foundation`, {
+    ...commonProps,
+  })
 
   const beehiveContext = getContextByNamespace('beehive')
   const beehivePipelineStack = new BeehivePipelineStack(app, `${namespace}-beehive-pipeline`, {
