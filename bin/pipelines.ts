72112df45e8eb8f68ca19f3b207192ec42f427af
Tom Hanstra <hanstra@nd.edu>
2020-11-19 10:05:40 -0500

    Add beehive pipeline

--- bin/pipelines.ts
+++ bin/pipelines.ts
@@ -3,5 +3,13 @@ import { CustomEnvironment } from '../lib/custom-environment'
 import { Stacks } from '../lib/types'
 
 export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment, testStacks: Stacks, prodStacks: Stacks): Stacks => {
+
+  const beehiveContext = getContextByNamespace('beehive')
+  const beehivePipelineStack = new BeehivePipelineStack(app, `${namespace}-beehive-pipeline`, {
+    foundationStack,
+    ...commonProps,
+    ...beehiveContext,
+  })
+
   return {}
 }
