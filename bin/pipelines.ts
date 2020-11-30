dd32e699fd3e991b814e339d158eb9a95c17fbaf
Tom Hanstra <hanstra@nd.edu>
2020-11-30 10:35:13 -0500

    Add Ian's pipelines.ts; will update with beehive soon

---
+++ bin/pipelines.ts
@@ -0,0 +1,37 @@
+import { App } from '@aws-cdk/core'
+import { CustomEnvironment } from '../src/custom-environment'
+import { Stacks } from '../src/types'
+import { getContextByNamespace } from '../src/context-helpers'
+import { FoundationStack } from '../src/foundation-stack'
+import { BuzzStack } from '../src/buzz/buzz-stack'
+import { BuzzPipelineStack } from '../src/buzz/buzz-pipeline'
+
+export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment, testStacks: Stacks, prodStacks: Stacks): Stacks => {
+  const infraRepoName = app.node.tryGetContext('infraRepoName')
+  const infraRepoOwner = app.node.tryGetContext('infraRepoOwner')
+  const infraSourceBranch = app.node.tryGetContext('infraSourceBranch')
+  const dockerCredentialsPath = app.node.tryGetContext('dockerCredentialsPath')
+
+  const commonProps = {
+    namespace,
+    env: env,
+    infraRepoOwner: infraRepoOwner,
+    infraRepoName: infraRepoName,
+    infraSourceBranch: infraSourceBranch,
+    dockerCredentialsPath: dockerCredentialsPath,
+  }
+
+  const foundationStack = new FoundationStack(app, `${namespace}-foundation`, {
+    ...commonProps,
+  })
+
+  const buzzContext = getContextByNamespace('buzz')
+  const buzzPipelineStack = new BuzzPipelineStack(app, `${namespace}-buzz-pipeline`, {
+    foundationStack,
+    testStack: testStacks.BuzzStack,
+    prodStack: prodStacks.BuzzStack,
+    ...commonProps,
+    ...buzzContext,
+  })
+  return { buzzPipelineStack }
+}
